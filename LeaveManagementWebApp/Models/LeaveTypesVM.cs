c92395d0634f57b80e37e3191fe03cc4fa041028
devyaniw <devyaniw@EON-DEVYANIW>
2023-10-09 04:53:04 -0700

    Added databasetables and migrations.

---
+++ LeaveManagementWebApp/Models/LeaveTypesVM.cs
@@ -0,0 +1,15 @@
+﻿using LeaveManagementWebApp.Data;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagementWebApp.Models
+{
+    public class LeaveTypesVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [Display(Name = "Default Number Of Days")]
+        public int DefaultDays { get; set; }
+    }
+}
